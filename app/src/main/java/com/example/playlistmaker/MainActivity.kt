c75166fcec917478aae9dc438954bb22e247bee4
Jormungandzxc <diborempty@gmail.com>
2026-07-01 21:29:26 +0200

    Реализация enableEdgeToEdge

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -9,21 +9,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.setPadding
 import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //Окраска статус бара и навигации
-        val windowController = ViewCompat.getWindowInsetsController(window.decorView)
-        val statusBarColor = getColor(R.color.blue_color)
-        val navigationBarColor = getColor(R.color.blue_color)
-        window.statusBarColor = statusBarColor
-        window.navigationBarColor = navigationBarColor
-        windowController?.isAppearanceLightStatusBars = true
-        windowController?.isAppearanceLightNavigationBars = true
+        //Реализация enableEdgeToEdge
+        val rootView = findViewById<View>(R.id.main)
+        ViewCompat.setOnApplyWindowInsetsListener(rootView){v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(0, systemBars.top, 0, systemBars.bottom)
+            insets
+        }
 
         //Обработка нажатий
         val searchButton = findViewById<MaterialButton>(R.id.btn_search)
